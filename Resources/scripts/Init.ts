6fb62946047fbe2ca3e04b17d9915ab1aa6ec6a0
Lukáš Chylík <lukas.chylik@riganti.cz>
2023-04-06 06:31:21 +0200

    fix

--- Resources/scripts/Init.ts
+++ Resources/scripts/Init.ts
@@ -1,10 +1,3 @@
 ﻿addEventListener("DOMContentLoaded", () => {
     console.log("Neuron Bootstrap colors")
-
-    let blueColors = document.querySelectorAll('.bg-primary')
-
-    for (let i = 1; i < 10; i++){
-        let shade = 100 * i
-        blueColors[i].classList.add('blue-' + shade)
-    }
 })
--- wwwroot/js/init.js
+++ wwwroot/js/init.js
@@ -1,17 +1,2 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!***********************************!*\
-  !*** ./Resources/scripts/init.ts ***!
-  \***********************************/
-addEventListener("DOMContentLoaded", () => {
-    console.log("Neuron Bootstrap colors");
-    let blueColors = document.querySelectorAll('.bg-primary');
-    for (let i = 1; i < 10; i++) {
-        let shade = 100 * i;
-        blueColors[i].classList.add('blue-' + shade);
-    }
-});
-
-/******/ })()
-;
+addEventListener("DOMContentLoaded",(()=>{console.log("Neuron Bootstrap colors")}));
 //# sourceMappingURL=init.js.map
